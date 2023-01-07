0ff1eefe4c510a3e146b943887352687a3e04eee
Programador Cego <programadorcego@gmail.com>
2023-01-07 13:13:13 -0300

    Inserindo som do alarme;

--- alarm/script.js
+++ alarm/script.js
@@ -1,25 +1,32 @@
-let hours = 1 * 60 * 60;
-let minutes = 0 * 60;
-let seconds = 0;
-let countDown = new Date(new Date().getTime() + ((hours + minutes + seconds) * 1000)).getTime();
+function countDown()
+{
+	let hours = 0 * 60 * 60 * 1000;
+	let minutes = 0 * 60 * 1000;
+	let seconds = 10 * 1000;
+	let countDown = new Date(new Date().getTime() + hours + minutes + seconds).getTime()
 
-let x = setInterval(() => {
-	let now = new Date().getTime();
-	let distance = countDown - now;
-	
-	hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-	seconds = Math.floor((distance % (1000 * 60)) / 1000);
-	
-	if(hours < 10) hours = hours + '0';
-	if(minutes < 10) minutes = minutes + '0';
-	if(seconds < 10) seconds = seconds + '0';
-	
-	document.querySelector('.countdown').innerHTML = `${hours}:${minutes}:${seconds}`;
-	
-	if(distance <0)
-	{
-		clearInterval(x);
-		document.querySelector('.countdown').innerHTML = '00:00:00';
-	}
-}, 1000);
+	let x = setInterval(() => {
+		let now = new Date().getTime();
+		let distance = countDown - now;
+		
+		hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		seconds = Math.floor((distance % (1000 * 60)) / 1000);
+		
+		if(hours < 10) hours = '0' + hours;
+		if(minutes < 10) minutes = '0' + minutes;
+		if(seconds < 10) seconds = '0' + seconds;
+		
+		document.querySelector('.countdown').innerHTML = `${hours}:${minutes}:${seconds}`;
+		
+		if(distance <0)
+		{
+			clearInterval(x);
+			document.querySelector('.countdown').innerHTML = '00:00:00';
+			
+			const alarm = new Audio('alarm.mp3');
+			alarm.loop = true;
+			alarm.play();
+		}
+	}, 1000);
+}
