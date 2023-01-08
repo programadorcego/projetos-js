fd811edff3c1ea0c9d70f58551d31396182e94a0
Programador Cego <programadorcego@gmail.com>
2023-01-08 13:33:41 -0300

    Fade: Efeito Fade Out

--- fade/script.js
+++ fade/script.js
@@ -1,7 +1,13 @@
-document.querySelector('button').addEventListener('click', function(){
-	let opacity = 0;
-	const container = document.querySelector('.container');
-	
+let btn = document.querySelector('button');
+let opacity = 0;
+const container = document.querySelector('.container');
+
+document.querySelector('.fade-in').addEventListener('click', function(){
+	fadeIn();
+});
+
+function fadeIn()
+{
 	let fade = setInterval(() => {
 		opacity = Number(getComputedStyle(container).getPropertyValue('opacity'));
 		
@@ -11,6 +17,32 @@ document.querySelector('button').addEventListener('click', function(){
 			container.style.opacity = opacity;
 		} else{
 			clearInterval(fade);
+			btn.classList.add('fade-out');
+			btn.classList.remove('fade-in');
+			fadeOut();
 		}
 	}, 200);
-});
+}
+
+function fadeOut()
+{
+	document.querySelector('.fade-out').addEventListener('click', function(){
+		let fade = setInterval(() => {
+		opacity = Number(getComputedStyle(container).getPropertyValue('opacity'));
+		
+		if(opacity > 0)
+		{
+			opacity -= 0.1;
+			container.style.opacity = opacity;
+		} else{
+			clearInterval(fade);
+			btn.classList.add('fade-in');
+			btn.classList.remove('fade-out');
+			
+			document.querySelector('.fade-in').addEventListener('click', function(){
+				fadeIn();
+			});
+		}
+	}, 200);
+	});
+}
