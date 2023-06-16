d1773681d5329619e7b6021d28f5283acff75614
Programador Cego <programadorcego@gmail.com>
2023-06-16 07:28:56 -0300

    Aula 02: Accordion

--- main.js
+++ main.js
@@ -1,53 +1,23 @@
-const tasks = [];
-const taskInput = document.querySelector("#taskInput");
-const addTaskButton = document.querySelector("#addTaskButton");
-const toDoList = document.querySelector("#toDoList");
+const buttons = document.querySelectorAll(".accordion-button");
 
-addTaskButton.addEventListener("click", () => {
-	tasks.push({
-		text: taskInput.value,
-		checked: false,
+buttons.forEach(button => {
+	button.addEventListener("click", e => {
+		buttons.forEach(button => {
+			button.setAttribute("aria-expanded", false);
+			button.classList = "accordion-button";
+			
+			const panel = document.querySelector(`#${button.getAttribute("aria-controls")}`);
+			panel.style.minHeight = 0;
+			panel.classList = "panel";
+		});
+		
+		const targetElement = e.target;
+		const panel = document.querySelector(`#${targetElement.getAttribute("aria-controls")}`);
+		
+		targetElement.classList.add("active");
+		targetElement.setAttribute("aria-expanded", true);
+		
+		panel.classList.add("expanded");
+		panel.style.minHeight = `${panel.scrollHeight}px`;
 	});
-	
-	taskInput.value = "";
-	renderTasks();
 });
-
-function renderTasks()
-{
-	toDoList.innerHTML = "";
-	
-	tasks.forEach((task, index) => {
-		const li = document.createElement("li");
-		
-		if(task.checked)
-		{
-			li.classList = "checked";
-		}
-		
-		const span = document.createElement("span");
-		span.textContent = task.text;
-		span.addEventListener("click", () => toggleChecked(index));
-		
-		const removeButton = document.createElement("span");
-		removeButton.textContent = `\u00D7`;
-		removeButton.setAttribute("role", "button");
-		removeButton.setAttribute("aria-label", "Remover");
-		removeButton.addEventListener("click", () => removeTask(index));
-		
-		li.append(span, removeButton);
-		toDoList.append(li);
-	});
-}
-
-function toggleChecked(index)
-{
-	tasks[index].checked = !tasks[index].checked;
-	renderTasks();
-}
-
-function removeTask(index)
-{
-	tasks.splice(index, 1);
-	renderTasks();
-}
