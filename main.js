aa04d2e7666e4e26f180e1b9e4bf79001dd9e9d0
Programador Cego <programadorcego@gmail.com>
2023-06-20 18:56:58 -0300

    Aula 06: Navegação pelas abas usando as setas

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const tabs = document.querySelectorAll(".tab");
+const tabs = Array.from(document.querySelectorAll(".tab"));
 const panels = document.querySelectorAll(".tabpanel");
 
 tabs.forEach(tab => {
@@ -23,4 +23,39 @@ tabs.forEach(tab => {
 			panel.classList.add("show");
 		}
 	});
+	
+	tab.addEventListener("keydown", e => {
+		let index = tabs.indexOf(e.target);
+		
+		switch(e.key)
+		{
+			case "ArrowLeft" :
+				e.preventDefault();
+				
+				if(index > 0)
+				{
+					selectTab(tabs[--index]);
+				}
+			break;
+			
+			case "ArrowRight" :
+				e.preventDefault();
+				
+				if(index < tabs.length - 1)
+				{
+					selectTab(tabs[++index]);
+				}
+			break;
+		}
+	});
 });
+
+function selectTab(tab)
+{
+	tabs.forEach(tab => {
+		tab.tabIndex = "-1";
+	});
+	
+	tab.tabIndex = "0";
+	tab.focus();
+}
