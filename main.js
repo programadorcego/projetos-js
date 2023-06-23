62f3851dd9816fa48599516f7ed62f8e451ed62c
Programador Cego <programadorcego@gmail.com>
2023-06-23 08:55:49 -0300

    Aula 09: Árvore de elementos

--- main.js
+++ main.js
@@ -1,26 +1,70 @@
-const startBtn = document.querySelector("#start");
-const progressBar = document.querySelector(".progressbar");
+const items = Array.from(document.querySelectorAll(".treeitem"));
 
-startBtn.addEventListener("click", move);
+items.forEach(item => {
+	item.addEventListener("keydown", handleKeydown);
+});
 
-function move()
+function handleKeydown(e)
 {
-	let width = 0;
-	progressBar.setAttribute("aria-valuenow", width);
-	progressBar.style.width = `${width}%`;
-	progressBar.textContent = `${width}%`;
-	let interval = setInterval(frame, 100);
+	const currentItem = e.target;
+	const currentItemIndex = items.indexOf(currentItem);
 	
-	function frame()
+	switch(e.key)
 	{
-		if(width >= 100)
-		{
-			return clearInterval(interval);
-		}
+		case "ArrowDown" :
+			e.preventDefault();
+			
+			if(currentItemIndex < items.length - 1)
+			{
+				const item = (items[currentItemIndex + 1].parentElement.parentElement.ariaExpanded == "false") ? items[currentItemIndex + 1].parentElement.parentElement.nextElementSibling : items[currentItemIndex + 1];
+				selectItem(item);
+			}
+		break;
 		
-		width += 1;
-		progressBar.setAttribute("aria-valuenow", width);
-		progressBar.style.width = `${width}%`;
-		progressBar.textContent = `${width}%`;
+		case "ArrowUp" :
+			e.preventDefault();
+			
+			if(currentItemIndex > 0)
+			{
+				const item = (items[currentItemIndex - 1].parentElement.parentElement.ariaExpanded == "false") ? items[currentItemIndex - 1].parentElement.parentElement : items[currentItemIndex - 1];
+				selectItem(item);
+			}
+		break;
+		
+		case "ArrowRight" :
+			e.preventDefault();
+			
+			if(currentItem.ariaExpanded == "false")
+			{
+				toggleItem(currentItem, true, `${currentItem.querySelector(".subtree").scrollHeight}px`);
+			}
+		break;
+		
+		case "ArrowLeft" :
+			e.preventDefault();
+			
+			if(currentItem.ariaExpanded == "true")
+			{
+				toggleItem(currentItem, false, 0);
+			}
+		break;
 	}
+}
+
+function selectItem(item)
+{
+	items.forEach(item => {
+		item.tabIndex = "-1";
+	});
+	
+	item.tabIndex = "0";
+	item.focus();
+}
+
+function toggleItem(item, expanded, height)
+{
+	item.ariaExpanded = expanded;
+	item.querySelector(".subtree").classList.toggle("expanded");
+	item.querySelector(".subtree").style.minhEight = height;
+	item.querySelector(".caret").classList.toggle("caret-down");
 }
